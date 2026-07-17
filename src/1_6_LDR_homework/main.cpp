e11b19f5bbdde2d372fc646de79afcf22bef33d5
Dmytro Solop <dmtsolop@gmail.com>
2026-07-17 14:20:00 +0300

    Added buttons pins and arrays of constants

--- src/1_6_LDR_homework/main.cpp
+++ src/1_6_LDR_homework/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+#define ATTN_BTN_PIN 7
+#define BIT_BTN_PIN 6
 #define LDR_PIN 4
 #define LED_PIN 3
 #define ADC_DELAY_MS 1000
@@ -9,6 +11,10 @@
 const float uRef = 3300.0;
 const float adcMax = 4095.0;
 int lastTime = 0;
+
+const int resolutions[] = {9, 10, 11, 12};
+const adc_attenuation_t attenFact[] = {ADC_0db, ADC_2_5db, ADC_6db, ADC_11db};
+const float uRefsMv[] = {1100.0, 1500.0, 2200.0, 3100.0};
 
 float calculateVoltage(int rawData)
 {
@@ -26,6 +32,8 @@ void setup()
     Serial.begin(115200);
     delay(1000);
 
+    pinMode(ATTN_BTN_PIN, INPUT_PULLUP);
+    pinMode(BIT_BTN_PIN, INPUT_PULLUP);
     pinMode(LED_PIN, OUTPUT);
     analogSetPinAttenuation(LDR_PIN, ADC_11db);
     lastTime = millis();
